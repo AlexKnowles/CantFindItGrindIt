e3ded184521965b8136c50f594dce1f0f3c1d6ba
Alex Knowles <alex@knowlesy.co.uk>
2018-01-27 01:15:17 +0000

    Basic prototype

---
+++ Assets/Code/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public LongPress AcceleratorButton;
+    public LongPress BreakButton;
+    public Slider RevCounter;
+
+    public float RevSpeed = 5f;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (AcceleratorButton.IsDown)
+        {
+            Accelerate();
+            return;
+        }
+
+        if (BreakButton.IsDown)
+        {
+            Break();
+        }
+
+        Decelerate();
+	}
+
+    private void Accelerate()
+    {
+        RevCounter.value += (RevSpeed * Time.deltaTime);
+    }
+
+    private void Break()
+    {
+        RevCounter.value -= (((RevSpeed / 2) * (1 + (RevCounter.value / RevCounter.maxValue))) * Time.deltaTime);
+    }
+    
+    private void Decelerate()
+    {
+        RevCounter.value -= (((RevSpeed/3) * (1 + (RevCounter.value/RevCounter.maxValue))) * Time.deltaTime);
+    }
+}
---
+++ Assets/Code/UI/LongPress.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LongPress : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+    public bool IsDown { get; private set; }
+    private float downTime;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        this.IsDown = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        this.IsDown = false;
+    }
+
+    void Update()
+    {
+    }
+
+}
